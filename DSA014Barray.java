9f0c952077b97283f037cb37df24511cf43eba95
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2021-09-06 12:33:30 +0530

    Most optimised 14 solution

--- DSA014Barray.java
+++ DSA014Barray.java
@@ -1,42 +1,34 @@
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Stack;
-public class DSA014Barray { //A Java program for merging overlapping intervals
-    public static void mergeIntervals(Interval arr[]) { //The main function that takes a set of intervals, merges overlapping intervals and prints the result
-        if (arr.length <= 0) return; //Test if the given set has at least one interval
-        Stack<Interval> stack = new Stack<>(); //Create an empty stack of intervals
-        Arrays.sort(arr, new Comparator<Interval>() { //sort the intervals in increasing order of start time
+public class DSA014Barray { //Java program to merge overlapping Intervals in O(n Log n) time and O(1) extra space
+    public static void mergeIntervals(Interval arr[]) { //Function that takes a set of intervals, merges  overlapping intervals and prints the result
+        Arrays.sort(arr, new Comparator<Interval>() { //Sort Intervals in increasing order of start time
             public int compare(Interval i1, Interval i2) {
                 return i1.start - i2.start;
             }
         });
-        stack.push(arr[0]); //push the first interval to stack
-        for (int i = 1; i < arr.length; i++) //Start from the next interval and merge if necessary
-        {
-            Interval top = stack.peek(); //get interval from stack top
-            if (top.end < arr[i].start) stack.push(arr[i]); //if current interval is not overlapping with stack top, push it to the stack
-            else if (top.end < arr[i].end) { //Otherwise update the ending time of top if ending of current interval is more
-                top.end = arr[i].end;
-                stack.pop();
-                stack.push(top);
+        int index = 0; //Stores index of last element in output array (modified arr[])
+        for(int i = 1; i < arr.length; i++) { //Traverse all input Intervals
+            if (arr[index].end >=  arr[i].start) { //If this is not first Interval and overlaps with the previous one
+                // Merge previous and current Intervals
+                arr[index].end = Math.max(arr[index].end, arr[i].end); 
+                arr[index].start = Math.min(arr[index].start, arr[i].start); 
             }
+            else arr[index++] = arr[i];
         }
-        System.out.print("The Merged Intervals are: "); // Print contents of stack
-        while (!stack.isEmpty()) {
-            Interval t = stack.pop();
-            System.out.print("[" + t.start + "," + t.end + "] ");
-        }
+        System.out.print("The Merged Intervals are: "); //Now arr[0..index-1] stores the merged Intervals 
+        for(int i = 0; i <= index; i++) System.out.print("[" + arr[i].start + ","+ arr[i].end + "]");
     }
     public static void main(String args[]) {
         Interval arr[] = new Interval[4];
-        arr[0] = new Interval(6, 8);
-        arr[1] = new Interval(1, 9);
-        arr[2] = new Interval(2, 4);
-        arr[3] = new Interval(4, 7);
+        arr[0] = new Interval(6,8);
+        arr[1] = new Interval(1,9);
+        arr[2] = new Interval(2,4);
+        arr[3] = new Interval(4,7);
         mergeIntervals(arr);
     }
 }
-class Interval {
+class Interval { //An Interval
     int start, end;
     Interval(int start, int end) {
         this.start = start;
